06b928887b54eb8c26436e48abfc4bebef88d49a
Pavel Mikhaylenko <104693653+westteern@users.noreply.github.com>
2022-12-16 14:09:42 +0200

    Datainput on hook

--- src/components/DataInput/DataInput.jsx
+++ src/components/DataInput/DataInput.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useState } from 'react';
 import {
   SubmitBtn,
   LableInput,
@@ -8,62 +8,66 @@ import {
   Input,
 } from './DataInput.styled';
 
-class DataInput extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+const DataInput = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  handleInput = e => {
-      const { name, value } = e.currentTarget;
-      this.setState({
-          [name]: value,
-      });
+  const handleInput = e => {
+    const { name, value } = e.target;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit(this.state);
-    this.reset();
+    onSubmit({ name, number });
+    resetValue();
   };
-  reset = () => {
-    this.setState({ name: '', number: '' })
+  const resetValue = () => {
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    return (
-      <SectionFormInput>
-        <FormInput onSubmit={this.handleSubmit}>
-          <LableInput>
-            Name
-            <Input
-              type="text"
-              name="name"
-              value={this.state.name}
-              onChange={this.handleInput}
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-          </LableInput>
+  return (
+    <SectionFormInput>
+      <FormInput onSubmit={handleSubmit}>
+        <LableInput>
+          Name
+          <Input
+            type="text"
+            name="name"
+            value={name}
+            onChange={handleInput}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </LableInput>
 
-          <LableInput>
-            Number
-            <Input
-              type="tel"
-              name="number"
-              value={this.state.number}
-              onChange={this.handleInput}
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-            />
-          </LableInput>
+        <LableInput>
+          Number
+          <Input
+            type="tel"
+            name="number"
+            value={number}
+            onChange={handleInput}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </LableInput>
 
-          <SubmitBtn type="submit">Add contact</SubmitBtn>
-        </FormInput>
-      </SectionFormInput>
-    );
-  };
+        <SubmitBtn type="submit">Add contact</SubmitBtn>
+      </FormInput>
+    </SectionFormInput>
+  );
 };
 
 DataInput.propTypes = {
